d23bb89a826297c3247dc971b1b2b8d4ea68d50a
Joey Tsui <joeytsui131@gmail.com>
2022-11-08 16:51:06 -0500

    "day2"

--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -1,17 +1,13 @@
 def my_map(arr, &proc)
     new_arr = []
-    arr.each do |ele|
-        new_arr << proc.call(ele)
-    end
+    arr.each {|ele| new_arr << proc.call(ele)}
     new_arr
 end
 
 def my_select(arr, &proc)
     new_arr = []
     arr.each do |ele|
-        if proc.call(ele)
-            new_arr << ele
-        end
+        new_arr << ele if proc.call(ele)
     end
     new_arr
 end
@@ -19,36 +15,28 @@ end
 def my_count (arr, &proc)
     count = 0
     arr.each do |ele|
-        if proc.call(ele)
-            count += 1
-        end
+        count += 1 if proc.call(ele)
     end
     return count
 end
 
 def my_any?(arr, &proc)
     arr.each do |ele|
-        if proc.call(ele)
-            return true
-        end
+       return true if proc.call(ele)
     end
     return false
 end
 
 def my_all?(arr, &proc)
     arr.each do |ele|
-        if !(proc.call(ele))
-            return false
-        end
+        return false if !(proc.call(ele))
     end
     return true
 end
 
 def my_none?(arr, &proc)
     arr.each do |ele|
-        if proc.call(ele)
-            return false
-        end
+        return false if proc.call(ele)
     end
     return true
 end
