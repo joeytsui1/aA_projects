cd65810c5c5e0026998136aa94e5256959016aeb
Joey Tsui <joeytsui131@gmail.com>
2022-11-08 11:46:08 -0500

    "day2"

---
+++ blocks_project/lib/part_1.rb
@@ -0,0 +1,24 @@
+def select_even_nums (arr)
+    even_num = arr.select(&:even?)
+end
+
+def reject_puppies(arr)
+    arr.reject {|hash| hash["age"] <= 2}
+end
+
+def count_positive_subarrays (arrays)
+    arrays.count {|array| array.sum > 0}
+end
+
+def aba_translate (str)
+    vowels = "aeiou"
+    new_str = ""
+    str.each_char.with_index do |char, i|
+        if vowels.include?(char)
+            new_str += str[i] + "b" + str[i]
+        else
+            new_str += str[i]
+        end
+    end
+    return new_str
+end
