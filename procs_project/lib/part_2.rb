6c6fa7e8569475b3b71b0653ee21d2599cfae74d
Joey Tsui <joeytsui131@gmail.com>
2022-11-08 16:56:16 -0500

    "day2"

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -3,17 +3,13 @@ def reverser(string, &proc)
 end
 
 def word_changer(string, &proc)
-    words = string.split(' ')
-    new_sen = words.map {|word| proc.call(word) }
-    new_sen.join(' ')
+    
+    new_sen = string.split(" ").map {|word| proc.call(word) }.join (' ')
+
 end
 
 def greater_proc_value(num, proc1, proc2)
-    if proc1.call(num) > proc2.call(num)
-        return proc1.call(num)
-    else
-        return proc2.call(num)
-    end
+    return proc1.call(num) > proc2.call(num) ? proc1.call(num) : proc2.call(num)
 end
 
 def and_selector (arr, proc1, proc2)
@@ -21,11 +17,6 @@ def and_selector (arr, proc1, proc2)
 end
 
 def alternating_mapper(arr, proc1, proc2)
-    arr.map.with_index do |ele, i|
-        if i.even?
-            proc1.call(ele)
-        else
-            proc2.call(ele)
-        end
-    end
+    arr.map.with_index  {|ele, i| i.even? ? proc1.call(ele) : proc2.call(ele)}
+    
 end
