70db2feb7dfa2709667b0460d50669fc44727a0d
Joey Tsui <joeytsui131@gmail.com>
2022-11-11 18:04:50 -0500

    "day5"

--- meddling_medley.rb
+++ meddling_medley.rb
@@ -132,6 +132,77 @@ end
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
 
+def convert_pig_latin(str)
+  vowels = 'aeiou'
+  words = str.split(' ')
+
+  words.map do |word|
+    if word.length >= 3
+      if vowels.include?(word[0].downcase)
+        word + 'yay'
+      else
+        vowel_index = vowel_finder(word)
+        if word[0] == word[0].upcase
+          (word[vowel_index..-1] + word[0..vowel_index] + 'ay').capitalize 
+        else
+          word[vowel_index..-1] + word[0..vowel_index] + 'ay' 
+        end
+      end
+    else
+      word
+    end
+  end.join(' ')
+end
+
+def vowel_finder(word) # => return index of the first vowel
+  vowels = 'aeiou'
+  word.each_char.with_index do |char, i|
+    return i if vowels.include?(char)
+  end
+end
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
+def reverberate (sen)
+    words = sen.split(" ")
+
+    new_sen = words.map do |word|
+        if word.length < 3
+            word
+        else
+            change_word!(word)
+        end
+    end
+    new_sen.join(" ")
+end
+
+
+
+def change_word! (word)
+    vowels = "aieou"
+    last_vowel_idx = 0
+
+    if vowels.include?(word[-1])
+        return word = word + word.downcase
+    end
+
+    word.each_char.with_index do |char, i|
+        if vowels.include?(char)
+            last_vowel_idx = i
+        end
+    end
+    return word = word + word[last_vowel_idx..-1]
+end
+
+# p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
+# p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
+# p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
+# p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
+
 
 def disjunct_select (arr, *procs)
     arr.select {|ele| procs.any? {|proc| proc.call(ele)}}
@@ -156,4 +227,134 @@ end
 #     starts_a
 # ) # ["ace", "dog", "apple", "teeming", "boot"]
 
+# def alternating_vowel(sen)
+#     words = sen.split(' ')
 
+#     new_sen = words.map.with_index do |word, i|
+#         first_index = find_index(word)
+#         last_index = word.length-1 - find_index(word.reverse)
+#         if find_index(word) != nil
+#             if i % 2 == 0 
+#                 word[0...first_index] + word[first_index+1 .. -1]
+#             else
+#                 word[0...last_index] + word[last_index+1..-1]
+#             end
+#         else
+#             word
+#         end
+#     end
+#     new_sen.join(' ')
+# end
+
+# def find_index(word)
+#     vowels = "aeiou"
+#     word.each_char.with_index do |char, i|
+#         if vowels.include?(char)
+#             return i
+#         end
+#     end
+# end
+
+def alternating_vowel(sent)
+  sentArr = sent.split(' ')
+
+  sentArr.each_with_index do |word, i|
+    if i % 2 == 0 
+      sentArr[i] = first_vowel_remove(word)
+    else
+      sentArr[i] = last_vowel_remove(word)
+    end
+  end
+  sentArr.join(" ")
+end
+
+def first_vowel_remove(word)
+  vowels = 'aeiou'
+
+  word.each_char.with_index do |char, i|
+    return word[0...i] + word[i + 1..-1] if vowels.include?(char.downcase)
+  end
+
+  word
+end
+
+def last_vowel_remove(word)
+  vowels = 'aeiou'
+
+  word.reverse!.each_char.with_index do |char, i|
+    if vowels.include?(char.downcase)
+      return (word[0...i] + word[i + 1..-1]).reverse
+    end
+  end 
+  word
+end
+
+
+
+# p alternating_vowel('panthers are great animals') # "pnthers ar grat animls"
+# p alternating_vowel('running panthers are epic') # "rnning panthrs re epc"
+# p alternating_vowel('code properly please') # "cde proprly plase"
+# p alternating_vowel('my forecast predicts rain today') # "my forecst prdicts ran tday"
+
+
+def silly_talk (str)
+    vowels = "aeiou"
+    words = str.split(' ')
+
+    new_sen = words.map do |word|
+        if vowels.include?(word[-1])
+            word + word[-1]
+        else
+            helper(word)
+        end
+    end
+    new_sen.join(' ')
+end
+
+def helper(word)
+    vowels = "aeiou"
+    new_str = ""
+    word.each_char.with_index do |char, i|
+        if vowels.include?(char.downcase)
+            new_str += char + "b"+ char.downcase
+        else
+            new_str += char
+        end
+    end
+    return new_str
+end
+
+# p silly_talk('Kids like cats and dogs') # "Kibids likee cabats aband dobogs"
+# p silly_talk('Stop that scooter') # "Stobop thabat scobooboteber"
+# p silly_talk('They can code') # "Thebey caban codee"
+# p silly_talk('He flew to Italy') # "Hee flebew too Ibitabaly"
+
+def compress (str)
+
+    new_str = ""
+    count = 0
+
+    str.each_char.with_index do |char,i|
+        if str[i] == str[i+1]
+            count += 1
+        else
+            hash[char] = count
+            count = 0
+        end
+    end
+
+    hash.each do |key, v|
+        if v == 1
+            new_str += key
+        else
+             new_str += key
+            new_str += v.to_s
+        end
+
+    end
+    return new_str
+end
+
+p compress('aabbbbc')   # "a2b4c"
+p compress('boot')      # "bo2t"
+p compress('xxxyxxzzzz')# "x3yx2z4"
