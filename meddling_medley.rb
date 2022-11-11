d3e49a7fc13e4eb7a26ae1dbb879f40fae83bebe
Joey Tsui <joeytsui131@gmail.com>
2022-11-11 14:43:15 -0500

    "day4"

---
+++ meddling_medley.rb
@@ -0,0 +1,159 @@
+def duos (str)
+    (0...str.length).count {|i| str[i] == str[i+1] }
+end
+
+# p duos('bootcamp')      # 1
+# p duos('wxxyzz')        # 2
+# p duos('hoooraay')      # 3
+# p duos('dinosaurs')     # 0
+# p duos('e')             # 0
+
+def sentence_swap (sen, hash)
+    words = sen.split(' ')
+    new_sen = words.map {|word| hash.has_key?(word) ? hash[word] : word}.join(' ')
+end
+
+# p sentence_swap('anything you can do I can do',
+#     'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+# ) # 'nothing you shall drink I shall drink'
+
+# p sentence_swap('what a sad ad',
+#     'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+# ) # 'make a happy ad'
+
+# p sentence_swap('keep coding okay',
+#     'coding'=>'running', 'kay'=>'pen'
+# ) # 'keep running okay'
+
+def hash_mapped (hash, proc, &blk)
+    new_hash = Hash.new {|hash, key| hash[key] = []}
+
+    hash.each do |key, value|
+        new_hash[blk.call(key)] = proc.call(value)
+    end
+    return new_hash
+end
+
+# double = Proc.new { |n| n * 2 }
+# p hash_mapped({'a'=>4, 'x'=>7, 'c'=>-3}, double) { |k| k.upcase + '!!' }
+# # {"A!!"=>8, "X!!"=>14, "C!!"=>-6}
+
+# first = Proc.new { |a| a[0] }
+# p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
+# # {25=>"q", 36=>"w"}
+
+
+def counted_characters (string)
+    hash = Hash.new(0)
+    new_arr = []
+    string.each_char {|char| hash[char] += 1}
+
+    hash.map do |key, value|
+        if value > 2
+            new_arr << key
+        end
+    end
+    return new_arr
+end
+
+# def counted_characters (string)
+#     hash = Hash.new(0)
+#     string.each_char {|char| hash[char] += 1}
+#     hash.keys.select {|key| hash[key] > 2}
+# end
+
+# p counted_characters("that's alright folks") # ["t"]
+# p counted_characters("mississippi") # ["i", "s"]
+# p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
+# p counted_characters("runtime") # []
+
+def triplet_true (str)
+    (0...str.length).any? {|i| str[i] == str[i+1] && str[i] == str[i+2]}
+end
+
+# p triplet_true('caaabb')        # true
+# p triplet_true('terrrrrible')   # true
+# p triplet_true('runninggg')     # true
+# p triplet_true('bootcamp')      # false
+# p triplet_true('e')             # false
+
+def energetic_encoding(str, hash)
+    new_str = ''
+    str.each_char do |char|
+        if hash.has_key?(char)
+            new_str += hash[char]
+        elsif char == " "
+            new_str += " "
+        else
+            new_str += "?"
+        end
+    end
+    return new_str
+end
+
+p energetic_encoding('sent sea',
+    'e'=>'i', 's'=>'z', 'n'=>'m', 't'=>'p', 'a'=>'u'
+) # 'zimp ziu'
+
+p energetic_encoding('cat',
+    'a'=>'o', 'c'=>'k'
+) # 'ko?'
+
+p energetic_encoding('hello world',
+    'o'=>'i', 'l'=>'r', 'e'=>'a'
+) # '?arri ?i?r?'
+
+p energetic_encoding('bike', {}) # '????']
+
+def uncompress(str)
+    new_str = ""
+    str.each_char.with_index do |char, i|
+        num = (str[i+1]).to_i
+        num.times do 
+            new_str += char
+        end
+    end
+    new_str
+end
+
+# p uncompress('a2b4c1') # 'aabbbbc'
+# p uncompress('b1o2t1') # 'boot'
+# p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+
+def conjunct_select (arr, *procs)
+    arr.select {|ele| procs.all? {|proc| proc.call(ele)} }
+end
+
+# is_positive = Proc.new { |n| n > 0 }
+# is_odd = Proc.new { |n| n.odd? }
+# less_than_ten = Proc.new { |n| n < 10 }
+
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
+
+def disjunct_select (arr, *procs)
+    arr.select {|ele| procs.any? {|proc| proc.call(ele)}}
+end
+
+# longer_four = Proc.new { |s| s.length > 4 }
+# contains_o = Proc.new { |s| s.include?('o') }
+# starts_a = Proc.new { |s| s[0] == 'a' }
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+# ) # ["apple", "teeming"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o
+# ) # ["dog", "apple", "teeming", "boot"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o,
+#     starts_a
+# ) # ["ace", "dog", "apple", "teeming", "boot"]
+
+
--- rspec_exercise_5/lib/exercise.rb
+++ rspec_exercise_5/lib/exercise.rb
@@ -1,24 +1,61 @@
 def zip (*arrs)
-    (0...arrs[0].length).map {|i| arrs.map {|arr| arr[i]}}
+    new_arr = []
+
+    arrs[0].each_with_index do |arr, i|
+        inner_arr = []
+        arrs.each {|arr| inner_arr << arr[i]}
+        new_arr << inner_arr
+    end
+    return new_arr
+    # (0...arrs[0].length).map {|i| arrs.map {|arr| arr[i]}}
 end
 
 def prizz_proc (arr, proc1, proc2)
     arr.select {|ele| (proc1.call(ele) || proc2.call(ele)) && !(proc1.call(ele) && proc2.call(ele))}
 end
 
-def zany_zip (*arrs)
-    (0...arrs.map(&:length).max).map {|i| arrs.map {|arr| arr[i]}}
+# def zany_zip (*arrs)
+#     (0...arrs.map(&:length).max).map {|i| arrs.map {|arr| arr[i]}}
+# end
+
+def zany_zip(*arrs)
+    length = arrs.inject(0) do |acc, arr|
+        if arr.length > acc
+            acc = arr.length
+        end
+        acc
+    end
+
+    new_arr = []
+    (0...length).each do |i|
+        inner_arr = []
+        arrs.each do |arr|
+        inner_arr << arr[i]
+        end
+        new_arr << inner_arr
+    end
+    return new_arr
 end
 
+# def maximum (arr, &proc)
+#     max = arr[0]
+#     arr.each do |ele|
+#         if proc.call(ele) >= proc.call(max)
+#             max = ele
+#         end
+#     end
+#     return max
+# end
+
 def maximum (arr, &proc)
-    max = arr[0]
-    arr.each do |ele|
-        if proc.call(ele) >= proc.call(max)
-            max = ele
+    arr.inject(arr.first) do |acc, ele|
+        if proc.call(ele) >= proc.call(acc)
+            acc = ele
         end
+        acc
     end
-    return max
 end
+
 
 def my_group_by (arr, &proc)
     new_hash = Hash.new {|hash, key| hash[key] = []} 
@@ -43,18 +80,29 @@ def max_tie_breaker (arr, proc, &blk)
     return max
 end
 
-def silly_syllables(sen)
-    words = sen.split(" ")
-    words.map {|word| remove_vowels(word)}.join(' ')
-end
+def silly_syllables(sentence)
+    words = sentence.split(' ')
+    new_sen = [] 
+    words.each do |word|
+        first_index = vowels_finder(word)[0]  
+        last_index = vowels_finder(word)[-1]
+        if first_index == last_index
+            new_sen << word
+        else 
+            new_sen << word[first_index ..last_index]
+        end 
+    end 
+    return new_sen.join(' ')        
+end 
 
-def remove_vowels(word)
-    vowels = "aeiou"
-
-    word.each_char.with_index do |char, i|
+    
+def vowels_finder(word)
+    vowels = 'aeiou'
+    arr = [] 
+    word.each_char.with_index do |char, i|  
         if vowels.include?(char)
-            word = word[0...i] + word[i+1..-1]
-        end
-    end
-end
-
+            arr << i
+        end 
+    end 
+    arr 
+end 
