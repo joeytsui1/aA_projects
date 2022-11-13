0b7f4350a4fb3ce4b18b0b8775c2d470218c9436
Joey Tsui <joeytsui131@gmail.com>
2022-11-13 15:55:45 -0500

    "day7"

--- nauseating_number 2.rb
+++ nauseating_number 2.rb
@@ -599,3 +599,152 @@ end
 # p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
 # p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
 # p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
+
+def strange_sums (nums)
+    count = 0
+
+    (0..nums.length-1).each do |i|
+        (i+1..nums.length-1).each do |j|
+            if nums[i] + nums[j] == 0
+                count += 1
+            end
+        end
+    end
+    return count
+end
+
+# p strange_sums([2, -3, 3, 4, -2])     # 2
+# p strange_sums([42, 3, -1, -42])      # 1
+# p strange_sums([-5, 5])               # 1
+# p strange_sums([19, 6, -3, -20])      # 0
+# p strange_sums([9])                   # 0
+
+def pair_product (nums, prod)
+    (0..nums.length-1).each do |i|
+        (i+1..nums.length-1).each do |j|
+            if nums[i] * nums[j] == prod
+                return true
+            end
+        end
+    end
+    return false
+end
+p "========="
+# p pair_product([4, 2, 5, 8], 16)    # true
+# p pair_product([8, 1, 9, 3], 8)     # true
+# p pair_product([3, 4], 12)          # true
+# p pair_product([3, 4, 6, 2, 5], 12) # true
+# p pair_product([4, 2, 5, 7], 16)    # false
+# p pair_product([8, 4, 9, 3], 8)     # false
+# p pair_product([3], 12)             # false
+
+def rampant_repeats (str, hash)
+    new_str = ""
+
+    str.each_char do |char| 
+        if hash.has_key?(char)
+            hash[char].times {new_str += char}
+        else
+            new_str += char
+        end
+    end
+    new_str
+end
+
+# p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+# p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+# p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+# p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+
+def perfect_square? (num)
+    (0...num).each do |i|
+        if i * i == num
+            return true
+        end
+    end
+    return false
+end
+
+# p perfect_square(1)     # true
+# p perfect_square(4)     # true
+# p perfect_square(64)    # true
+# p perfect_square(100)   # true
+# p perfect_square(169)   # true
+# p perfect_square(2)     # false
+# p perfect_square(40)    # false
+# p perfect_square(32)    # false
+# p perfect_square(50)    # false
+
+def anti_prime? (num)
+    (1...num).none? {|i| factors(i) > factors(num)}
+end
+
+def factors (num)
+    (1...num).count {|i| num % i == 0}
+end
+
+# p anti_prime?(24)   # true
+# p anti_prime?(36)   # true
+# p anti_prime?(48)   # true
+# p anti_prime?(360)  # true
+# p anti_prime?(1260) # true
+# p anti_prime?(27)   # false
+# p anti_prime?(5)    # false
+# p anti_prime?(100)  # false
+# p anti_prime?(136)  # false
+# p anti_prime?(1024) # false
+
+def matrix_addition (m1, m2)
+    new_m = Array.new(m1.length) {Array.new()}
+
+    (0..m1.length-1).each do |i|
+        (0..m1[0].length-1).each do |j|
+            new_m[i][j] = m1[i][j] + m2[i][j]
+        end
+    end
+    return new_m
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+
+def mutual_factors (*nums)
+    (1..nums.min).select {|i| nums.all? {|num| num % i == 0}} 
+end
+
+# p mutual_factors(50, 30)            # [1, 2, 5, 10]
+# p mutual_factors(50, 30, 45, 105)   # [1, 5]
+# p mutual_factors(8, 4)              # [1, 2, 4]
+# p mutual_factors(8, 4, 10)          # [1, 2]
+# p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+# p mutual_factors(12, 24, 64)        # [1, 2, 4]
+# p mutual_factors(22, 44)            # [1, 2, 11, 22]
+# p mutual_factors(22, 44, 11)        # [1, 11]
+# p mutual_factors(7)                 # [1, 7]
+# p mutual_factors(7, 9)              # [1]
+
+def tribonacci_number (num)
+    seq = [1,1,2]
+
+    while seq.length < num
+        seq << seq[-3] + seq[-2] + seq[-1]
+    end
+    return seq[num-1]
+end
+
+# p tribonacci_number(1)  # 1
+# p tribonacci_number(2)  # 1
+# p tribonacci_number(3)  # 2
+# p tribonacci_number(4)  # 4
+# p tribonacci_number(5)  # 7
+# p tribonacci_number(6)  # 13
+# p tribonacci_number(7)  # 24
+# p tribonacci_number(11) # 274
