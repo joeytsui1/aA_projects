8d9700578eaea984c7ae43d94fd1376a9ad00519
Joey Tsui <joeytsui131@gmail.com>
2022-11-15 16:45:11 -0500

    "day7"

---
+++ Week2/w2d2/guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,36 @@
+# require_relative "./play_guessing_game.rb"
+
+class GuessingGame
+
+    attr_reader :num_attempts
+    attr_accessor 
+
+    def initialize (min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num (num)
+        @num_attempts += 1
+        
+        if num == @secret_num
+            @game_over = true
+            puts "you win"
+        elsif num > @secret_num
+            puts "too big"
+        else
+            puts "too small"
+        end
+    end
+
+    def ask_user
+        puts "enter a number"
+        num_guess = gets.chomp.to_i
+        check_num(num_guess)
+    end
+end
